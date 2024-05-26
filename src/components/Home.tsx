6736963fc7e10c710322ce27c5f88bb3722e9edf
samarth8765 <samdhawan8765@gmail.com>
2024-05-26 22:33:51 +0530

    updated Home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,11 +4,13 @@ export default function Home(): React.JSX.Element {
   const navigate = useNavigate();
   return (
     <div>
-      <h1>Forests and their management</h1>
-      <h2>Number of questions: 120</h2>
-      <p>Description blah blah blah</p>
+      <h1 style={{ textAlign: "center" }}>Forests and their management</h1>
+      <h2 style={{ textAlign: "center" }}>Number of questions: 120</h2>
+      <h1 style={{ textAlign: "center", color: "red" }}>Chaos is a ladder</h1>
 
-      <button onClick={() => navigate("/quiz")}>Start Quiz</button>
+      <button onClick={() => navigate("/quiz")} style={{ marginLeft: "47%" }}>
+        Start Quiz
+      </button>
     </div>
   );
 }
